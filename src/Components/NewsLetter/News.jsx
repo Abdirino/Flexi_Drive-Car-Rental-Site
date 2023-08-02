d958787e18be4ac699b7bbfb29236f3cb6e78f8d
Abdirino <abdirinoahash@gmail.com>
2023-08-02 09:41:44 +0300

    commencing ia

--- src/Components/NewsLetter/News.jsx
+++ src/Components/NewsLetter/News.jsx
@@ -4,9 +4,17 @@ import './news.css'
 const News = () => {
   return (
     <>
-        <div className="News">
-            <div className="news"></div>
+      <div className="News">
+        <div className="news">
+          <div className="n-detail">
+            <h1>Save big with our cheap car rental!</h1>
+            <p>Top Airports. Local Suppliers. 24/7 Support.</p>
+          </div>
+          <div className="n-button">
+            <button>Book Ride!</button>
+          </div>
         </div>
+      </div>
     </>
   )
 }
