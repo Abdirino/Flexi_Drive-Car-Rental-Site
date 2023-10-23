99ab486789ebbaf787101d42e4d736c0145d4538
Abdirahaman Abdulaziz <abdirinoahash@gmail.com>
2023-10-23 13:46:44 +0300

    React countUp Add ✅

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -1,62 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import "./pricing.css";
 
 import StoreIMG from "../../Images/Flexi_Store IMG.png";
+import ScrollTrigger from "react-scroll-trigger";
+import CountUp from "react-countup";
 
 const Pricing = () => {
+  const [counterOn, setCounterOn] = useState(false);
+
   return (
     <>
-      <div className="facts">
-        <div className="head">
-          <h1>Facts By The Numbers</h1>
-          <div></div>
-          <div></div>
+      <ScrollTrigger
+        onEnter={() => setCounterOn(true)}
+        onExit={() => setCounterOn(false)}
+      >
+        <div className="facts">
+          <div className="head">
+            <h1>Facts By The Numbers</h1>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="Fact">
+            <div className="fact">
+              <div className="icon">
+                <i class="fa-regular fa-heart"></i>
+              </div>
+              <div className="detail">
+                <h2>
+                  {counterOn && (
+                    <CountUp start={0} end={16} delay={0} duration={2} />
+                  )}
+                  K+
+                </h2>
+                <p>Happy Customers</p>
+              </div>
+            </div>
+            <div className="fact">
+              <div className="icon">
+                <i class="fa-solid fa-car"></i>
+              </div>
+              <div className="detail">
+                <h2>
+                  {counterOn && (
+                    <CountUp start={0} end={254} delay={0} duration={2} />
+                  )}
+                  +
+                </h2>
+                <p>Count Of Cars</p>
+              </div>
+            </div>
+            <div className="fact">
+              <div className="icon">
+                <i class="fa-solid fa-headphones"></i>
+              </div>
+              <div className="detail">
+                <h2>
+                  {counterOn && (
+                    <CountUp start={0} end={625} delay={0} duration={2} />
+                  )}
+                  +
+                </h2>
+                <p>Car Center Solutions</p>
+              </div>
+            </div>
+            <div className="fact">
+              <div className="icon">
+                <i class="fa-solid fa-gauge-high"></i>
+              </div>
+              <div className="detail">
+                <h2>
+                  {counterOn && (
+                    <CountUp start={0} end={600} delay={0} duration={2} />
+                  )}
+                  K+
+                </h2>
+                <p>Total Kilometers</p>
+              </div>
+            </div>
+          </div>
         </div>
-        {/* <div className="content">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto
-            eligendi deserunt officia!
-          </p>
-        </div> */}
-        <div className="Fact">
-          <div className="fact">
-            <div className="icon">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div className="detail">
-              <h2>16K+</h2>
-              <p>Happy Customers</p>
-            </div>
-          </div>
-          <div className="fact">
-            <div className="icon">
-              <i class="fa-solid fa-car"></i>
-            </div>
-            <div className="detail">
-              <h2>2547+</h2>
-              <p>Count Of Cars</p>
-            </div>
-          </div>
-          <div className="fact">
-            <div className="icon">
-              <i class="fa-solid fa-headphones"></i>
-            </div>
-            <div className="detail">
-              <h2>625K+</h2>
-              <p>Car Center Solutions</p>
-            </div>
-          </div>
-          <div className="fact">
-            <div className="icon">
-              <i class="fa-solid fa-gauge-high"></i>
-            </div>
-            <div className="detail">
-              <h2>200K+</h2>
-              <p>Total Kilometers</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      </ScrollTrigger>
 
       <div className="application">
         <div className="details">
