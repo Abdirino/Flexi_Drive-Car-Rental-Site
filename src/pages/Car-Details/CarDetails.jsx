8cdf31288d70039bdb79746f279aabe090418c05
Abdirahaman Abdulaziz <abdirinoahash@gmail.com>
2023-11-02 09:14:42 +0300

    splide slider add ✅

--- src/pages/Car-Details/CarDetails.jsx
+++ src/pages/Car-Details/CarDetails.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./car-details.css";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
-// import { Carousel } from "react-responsive-carousel";
+// import "@splidejs/react-splide/css";
+
+import "@splidejs/react-splide/css/skyblue";
+// import "@splidejs/react-splide/css/sea-green";
 
 import Footer from "../../Components/Footer/Footer";
 import Navbar from "../../Components/Navbar/Navbar";
 
 import Hero from "../../Images/Car-details.jpg";
 import Booking from "../../Components/Booking/Booking";
-// import car1 from "../../Images/car-01.jpg";
-// import car2 from "../../Images/car-02.jpg";
-// import car3 from "../../Images/car-03.jpg";
-// import car4 from "../../Images/car-04.jpg";
-// import car5 from "../../Images/car-05.jpg";
-// import car6 from "../../Images/car-06.jpg";
-// import car7 from "../../Images/car-07.jpg";
+import car1 from "../../Images/car-01.jpg";
+import car2 from "../../Images/car-02.jpg";
+import car3 from "../../Images/car-03.jpg";
+import car4 from "../../Images/car-04.jpg";
+import car5 from "../../Images/car-05.jpg";
+import car6 from "../../Images/car-06.jpg";
+import car7 from "../../Images/car-07.jpg";
 
 const CarDetails = () => {
   return (
@@ -27,7 +30,6 @@ const CarDetails = () => {
       </div>
 
       <div className="car-details">
-        
         <div className="des">
           <div className="stars">
             <i class="fa-solid fa-star"></i>
@@ -46,26 +48,86 @@ const CarDetails = () => {
         </div>
 
         <div className="car-app">
+          <div className="car-slider">
+            <Splide
+              options={{
+                rewind: true,
+                width: 800,
+                gap: "1rem",
+                autoplay: true,
+                speed: 100,
+                pauseOnHover: true,
+              }}
+            >
+              <SplideSlide>
+                <img src={car1} alt="" />
+              </SplideSlide>
+              <SplideSlide>
+                <img src={car2} alt="" />
+              </SplideSlide>
+              <SplideSlide>
+                <img src={car3} alt="" />
+              </SplideSlide>
+              <SplideSlide>
+                <img src={car4} alt="" />
+              </SplideSlide>
+              <SplideSlide>
+                <img src={car5} alt="" />
+              </SplideSlide>
+              <SplideSlide>
+                <img src={car6} alt="" />
+              </SplideSlide>
+              <SplideSlide>
+                <img src={car6} alt="" />
+              </SplideSlide>
+              <SplideSlide>
+                <img src={car7} alt="" />
+              </SplideSlide>
+            </Splide>
+            <div className="Detail-specs">
+              <h1>Specifications</h1>
+              <div className="d-spec">
+                <p>Body</p>
+                <span>Ferrari</span>
+              </div>
+              <div className="d-spec">
+                <p>Seats</p>
+                <span>2 Seats</span>
+              </div>
+              <div className="d-spec">
+                <p>Laguege</p>
+                <span>170</span>
+              </div>
+              <div className="d-spec">
+                <p>Engine</p>
+                <span>3000</span>
+              </div>
+            </div>
+          </div>
 
           <div className="car-application">
             <div className="head">
-              <h2>Check <span>Availability</span></h2>
+              <h2>
+                Check <span>Availability</span>
+              </h2>
               <div></div>
             </div>
             <h3>Pick up Location</h3>
-            <input type="text" placeholder="45 4th Avenue Joggoo street Nairobi"/>
+            <input
+              type="text"
+              placeholder="45 4th Avenue Joggoo street Nairobi"
+            />
             <h3>Drop off Location</h3>
-            <input type="text" placeholder="78 10th Street Kajuja Nairobi"/>
+            <input type="text" placeholder="78 10th Street Kajuja Nairobi" />
             <h3>Pick Up Date</h3>
-            <input type="date" placeholder="4 / 11 / 2023"/>
+            <input type="date" placeholder="4 / 11 / 2023" />
             <h3>Return Date</h3>
-            <input type="date" placeholder="28 / 12 / 2023"/>
+            <input type="date" placeholder="28 / 12 / 2023" />
 
             <button>Check Availability</button>
           </div>
-          <Booking />
         </div>
-
+        <Booking />
       </div>
 
       <Footer />
